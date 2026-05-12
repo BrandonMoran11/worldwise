43ded43931971a23c5cbcd959eb7b3e817037cb7
BrandonMoran11 <brandon.mv.110203@gmail.com>
2026-05-12 14:23:19 -0600

    feat: scaffold project structure with base components, page layouts, and CSS modules

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,40 @@
-import PageNav from "../components/PageNav";
+import styles from "./Login.module.css";
+import { useState } from "react";
+import PageNav from "../components/PageNav.jsx";
 
-function Login() {
+export default function Login() {
+  // PRE-FILL FOR DEV PURPOSES
+  const [email, setEmail] = useState("jack@example.com");
+  const [password, setPassword] = useState("qwerty");
+
   return (
-    <div>
+    <main className={styles.login}>
       <PageNav />
-      <h1>Login</h1>
-    </div>
+      <form className={styles.form}>
+        <div className={styles.row}>
+          <label htmlFor="email">Email address</label>
+          <input
+            type="email"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </div>
+
+        <div className={styles.row}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </div>
+
+        <div>
+          <button>Login</button>
+        </div>
+      </form>
+    </main>
   );
 }
-
-export default Login;
